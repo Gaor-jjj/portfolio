57956407405c5638b03771d6c3fd4ad17be0179b
Gaor-jjj <kaurtamm1995@gmail.com>
2024-02-28 08:02:44 +0200

    Shorter script

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+        entry.target.classList.toggle('show', entry.isIntersecting);
     });
 });
 
