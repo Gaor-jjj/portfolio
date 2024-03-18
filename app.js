c940b0e32dc0acc6ab8b1d1d2f151254e8d2f210
Gaor-jjj <kaurtamm1995@gmail.com>
2024-03-18 16:36:15 +0200

    Different approach mobile/tablet
    Removed the previous changes in javascript and went with the CSS solution instead to still play animations on most tablets.

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+    });
+});
 
-if (!isMobileDevice()) {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            entry.target.classList.toggle('show', entry.isIntersecting);
-        });
-    });
-  
-    const hiddenElements = document.querySelectorAll('.hidden');
-    hiddenElements.forEach((el) => observer.observe(el));
-} else {
-    const hiddenElements = document.querySelectorAll('.hidden');
-    hiddenElements.forEach((el) => {
-        el.classList.remove('hidden');
-        el.classList.add('show');
-    });
-}
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
