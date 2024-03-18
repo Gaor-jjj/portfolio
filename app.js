bead3676f94d80e79c4ffb298574cd93c3a32b37
Gaor-jjj <kaurtamm1995@gmail.com>
2024-03-18 16:28:46 +0200

    Update script for mobile/tablet
    Disabled animations for mobile and tablet devices.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        entry.target.classList.toggle('show', entry.isIntersecting);
-    });
-});
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+if (!isMobileDevice()) {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            entry.target.classList.toggle('show', entry.isIntersecting);
+        });
+    });
+  
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+} else {
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => {
+        el.classList.remove('hidden');
+        el.classList.add('show');
+    });
+}
